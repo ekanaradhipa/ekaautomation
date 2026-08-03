d81112f45923eaafe4591524d0945f08be71c6c0
Ekanaradhipa Djacaria <ekanaradhipad@gmail.com>
2026-08-03 11:20:02 +0700

    change to snake and temporarily add testng path to run test

--- ekaautomationng/src/main/java/id/co/juaracoding/testng/TextUtilTest.java
+++ ekaautomationng/src/main/java/id/co/juaracoding/testng/TextUtilTest.java
@@ -1,86 +1,100 @@
 package id.co.juaracoding.testng;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static id.co.juaracoding.util.TextUtil.countVowels;
 import static id.co.juaracoding.util.TextUtil.isPalindrome;
 
 public class TextUtilTest {
+
+    @BeforeClass
+    public void before_class_method() {
+        System.out.println("Test Dimulai");
+    }
+
     @Test
-    public void testIsPalindrome() {
+    public void test_is_palindrome() {
         String input = "tacocat";
         boolean result = isPalindrome(input);
         Assert.assertTrue(result);
     }
 
     @Test
-    public void testIsPalindromeMixedCase() {
+    public void test_is_palindrome_mixed_case() {
         String input = "TacoCat";
         boolean result = isPalindrome(input);
         Assert.assertTrue(result);
     }
 
     @Test
-    public void testIsNotPalindrome() {
+    public void test_is_not_palindrome() {
         String input = "hello";
         boolean result = isPalindrome(input);
         Assert.assertFalse(result);
     }
 
     @Test
-    public void testIsNotPalindromeMixedCase() {
+    public void test_is_not_palindrome_mixed_case() {
         String input = "Hello";
         boolean result = isPalindrome(input);
         Assert.assertFalse(result);
     }
 
-    public void testIsPalindromeEmpty() {
+    @Test
+    public void test_is_palindrome_empty() {
         String input = "";
         boolean result = isPalindrome(input);
         Assert.assertTrue(result);
     }
 
-    public void testIsPalindromeNull() {
+    @Test
+    public void test_is_palindrome_null() {
        
         boolean result = isPalindrome(null);
         Assert.assertFalse(result);
     }
     
     @Test
-    public void testCountVowels() {
+    public void test_count_vowels() {
         String input = "acute";
         int result = countVowels(input);
         Assert.assertEquals(result, 3);
     }
 
     @Test
-    public void testCountVowelsMixedCase() {
+    public void test_count_vowels_mixed_case() {
         String input = "Acute";
         int result = countVowels(input);
         Assert.assertEquals(result, 3);
     }
 
     @Test
-    public void testCountVowelsZero() {
+    public void test_count_vowels_zero() {
         String input = "crypt";
         int result = countVowels(input);
         Assert.assertEquals(result, 0);
     }
 
     @Test
-    public void testCountVowelsEmpty() {
+    public void test_count_vowels_empty() {
         String input = "";
         int result = countVowels(input);
         Assert.assertEquals(result, 0);
     }
 
     @Test
-    public void testCountVowelsNull() {
+    public void test_count_vowels_null() {
         int result = countVowels(null);
         Assert.assertEquals(result, 0);
     }
 
+    @AfterClass
+    public void after_class_method() {
+        System.out.println("Test Selesai");
+    }
     
 
 }
